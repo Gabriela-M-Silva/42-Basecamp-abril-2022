2d04d487f73cd5d2c986b24175c577a64e787b1a
Gabriela de Moraes da Silva <90937264+Gabriela-M-Silva@users.noreply.github.com>
2022-04-27 00:44:23 -0300

    Lista C06

---
+++ C06/ex00/ft_sort_params.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-mora <gde-mora@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 08:51:44 by gde-mora          #+#    #+#             */
+/*   Updated: 2022/04/23 08:52:13 by gde-mora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	(void) argc;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+}
