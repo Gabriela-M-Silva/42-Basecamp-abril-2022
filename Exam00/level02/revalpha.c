4f15e8e1a12ab51b73f627599476631b837ac243
Gabriela de Moraes da Silva <90937264+Gabriela-M-Silva@users.noreply.github.com>
2022-05-01 22:09:04 -0300

    Update revalpha.c

--- Exam00/level02/revalpha.c
+++ Exam00/level02/revalpha.c
@@ -1,1 +1,24 @@
+#include <unistd.h>
 
+void revalpha(void)
+{
+  char  letter;
+
+  letter = 'z';
+  while (letter >= 'a')
+  {
+    if (letter >= 'a' && letter <= 'z')
+    {
+      write(1, &letter, 1);
+      letter -= 1;
+      letter = letter - 32;
+    }
+    if (letter >= 'A' && letter <= 'Z')
+    {
+      write(1, &letter, 1);
+      letter -= 1;
+      letter = letter + 32;
+    }
+  }
+  write(1, "\n", 1);
+}
